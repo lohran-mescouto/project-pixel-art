788e3fda7bd8a16c20843494c4f4370f01df3398
Lohran <lohran.albuquerque@gmail.com>
2023-02-07 20:41:24 -0300

    criando paleta de cores. requisito 2.

---
+++ script.js
@@ -0,0 +1,6 @@
+document.getElementsByClassName('color')[0].style.backgroundColor = 'black'
+document.getElementsByClassName('color')[1].style.backgroundColor = 'red'
+document.getElementsByClassName('color')[2].style.backgroundColor = 'yellow'
+document.getElementsByClassName('color')[3].style.backgroundColor = 'blue'
+
+
