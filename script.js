7150a75b92b4ba6f745a6e424590960389799560
Lohran <lohran.albuquerque@gmail.com>
2023-02-10 15:21:07 -0300

    adicionando quadro pixel e seu estilo. Requisito 6 e 7.

--- script.js
+++ script.js
@@ -13,20 +13,19 @@ document.getElementsByClassName('color')[3].style.backgroundColor = 'blue';
 const randomColors = () => {
   const paleta = document.getElementsByClassName('color');
   const botao = document.getElementById('button-random-color');
-  let cores = [];
+  const cores = [];
 
   botao.addEventListener('click', () => {
     for (let index = 1; index < paleta.length; index += 1) {
       const random = Math.floor(Math.random() * 16777215).toString(16); // Realizado com ajuda da monitora Natália.
       paleta[index].style.backgroundColor = `#${random}`;
       cores[index] = paleta[index].style.backgroundColor;
-      
       // localStorage.setItem('colorPalette', JSON.stringify(paleta[index].style.backgroundColor));
       // const salvedPalette = localStorage.getItem('colorPalette', JSON.stringify(paleta[index].style.backgroundColor));
       // console.log(salvedPalette);
-    };
+    }
     localStorage.setItem('colorPalette', JSON.stringify(cores));
-  })
+  });
 };
 
 const fixedColors = () => {
@@ -38,10 +37,23 @@ const fixedColors = () => {
     }
   }
 };
-
-
 randomColors();
 fixedColors();
+
+const quadroPixel = document.getElementById('pixel-board');
+for (let index = 0; index < 5; index += 1) {
+  const linhaPixel = document.createElement('div');
+  linhaPixel.classList.add('linhaPixel');
+
+  for (let i = 0; i < 5; i +=1) {
+    const quadro = document.createElement('div');
+    quadro.classList.add('pixel')
+    // quadro.style.backgroundColor = 'white';
+
+    linhaPixel.appendChild(quadro);
+  }
+  quadroPixel.appendChild(linhaPixel);
+}
+
 // const savedColors = localStorage.getItem('colorPalette');
 // document.getElementsByClassName('color').style.backgroundColor = savedColors;
-
